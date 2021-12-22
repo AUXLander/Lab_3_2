5802e7573e478e637f4ee620d32a61d146463297
Denis <36292357+AUXLander@users.noreply.github.com>
2021-12-22 22:10:46 +0300

    before optimization

--- Lab_3_2/Soution.cpp
+++ Lab_3_2/Soution.cpp
@@ -1,7 +1,17 @@
-#include "Soution.h"
+//#include "Soution.h"
 
 #include <iostream>
+#include "Intera.h"
+#include <math.h>
+#include <algorithm>
 
+double f(double x) {
+	return 2 * cos(2 * x) + 2;
+}
+double F(double x) {
+	return sin(2 * x) + 2 * x;
+}
+/*
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -16,22 +26,25 @@ int main(cli::array<String^>^ arg) {
 
 	return 0;
 }
+*/
 
+constexpr size_t N = 1'000'000;
 
-/*
-int main() {
+int main() 
+{
+	std::vector<point> squareStairs = Integral::squareStairs(f, 0.0, 1.0, N);
+	double result_NM = Integral::Numeric(f, 0.0, 1.0, N);
 
+	std::vector<point> uniformSquare = Integral::uniformRandomOnSquare({ 0.0, 1.0 }, { 1.0, 4.0 }, N);
+	double ratio_v1 = Integral::ratioIn(f, uniformSquare);
+	double result_v1 = Integral::MonteCarlo_V1(f, 0.0, 1.0, uniformSquare);
 	
-
-
-
-	std::cout << Integral::Numeric(f, 0, 1, 100000) << std::endl;
+	std::vector<double> uniformLine = Integral::uniformRandomOnLine(0.0, 1.0, N);
+	double result_v2 = Integral::MonteCarlo_V2(f, uniformLine);
 	
-	std::cout <<  << std::endl;
-	//std::cout << "Min: " << Integral::min << std::endl;
-	//std::cout << "Max: " << Integral::max << std::endl;
+	std::cout << result_NM << std::endl;
+	std::cout << result_v1 << std::endl;
+	std::cout << result_v2 << std::endl;
 
-	system("pause");
 	return 0;
 }
-*/
